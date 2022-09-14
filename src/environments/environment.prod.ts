e4e40e890aafd5a72c723e967258a825b66da9fd
Alex Jonguitud <ajungitud@itechdev.com.mx>
2022-09-13 19:10:35 -0500

    Api Bot Production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
     API_URL: 'https://baseitechfunction.azurewebsites.net',
-    apiURL: 'http://whatsappitech.azurewebsites.net/api'
+    //apiURL: 'http://whatsappitech.azurewebsites.net/api'desa
+    apiURL: 'https://botwhatappapiprod.azurewebsites.net/'
 };
