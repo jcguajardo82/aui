9591aaf53f43ac4f8bd9a7be5e9cbf2ed6007067
Julio <jlopez@itechdev.com.mx>
2022-09-15 17:14:13 -0500

    se le fue una coma

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     API_URL: 'https://baseitechfunction.azurewebsites.net',
-    ppsApiUrl : 'http://localhost:55564/api'
+    ppsApiUrl : 'http://localhost:55564/api',
     apiURL: 'http://whatsappitech.azurewebsites.net/api',
 };
